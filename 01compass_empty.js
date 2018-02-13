1485ac8231ab4ffb8b6d4576f7b5c3c412021762
Jennifer Muraoka <jmurao@saic.edu>
2018-02-13 14:26:13 -0600

    added direction headings

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -9,19 +9,19 @@ oriented = function(e) {
       }
 // north
       if (compassHeading > 315 || compassHeading < 45) {
-        text_area.textContent = compassHeading;
+        text_area.textContent = "north";
       }
 // east
       if (compassHeading >= 45 && compassHeading < 135) {
-        text_area.textContent = compassHeading;
+        text_area.textContent = "east";
       }
 //south
       if (compassHeading >= 135 && compassHeading < 225) {
-        text_area.textContent = compassHeading;
+        text_area.textContent = "south";
       }
 // west
       if (compassHeading >= 225 && compassHeading <= 315) {
-        text_area.textContent = compassHeading;
+        text_area.textContent = "west";
       }
        //calls function to alter content based on heading
        // myOrientation();

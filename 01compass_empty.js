a4d3a6aff2b059d62f32ce020ced38b452534e0b
Jennifer Muraoka <jmurao@saic.edu>
2018-02-18 01:36:21 -0600

    changed 'hawaii' and 'japan' messages. test

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -7,25 +7,52 @@ oriented = function(e) {
       } else {
         compassHeading = e.alpha;
       }
-// north
-      if (compassHeading > 315 || compassHeading < 45) {
-        text_area.textContent = "寒い";
+
+/* none of these are done
+labels are just so that you know where to look up
+for locations!  */
+
+
+// hawaii
+    if (compassHeading >= 205 && compassHeading < 270) {
+        text_area.textContent = "e komo mai howzit braddah da sun wen come in bright today yeah";
       }
-// east
+
+// japan
       if (compassHeading >= 45 && compassHeading < 135) {
-        text_area.textContent = "帰場所";
+        text_area.textContent = "始めまして、";
+        document.write("<br>");
+        text_area.textContent = "村岡。。。美佳？ジェン？アーヌ？　と申します。";
       }
-//south
+//minnesota
       if (compassHeading >= 135 && compassHeading < 225) {
         text_area.textContent = "熱い";
       }
-// west
+// in between hawaii and japan
       if (compassHeading >= 225 && compassHeading <= 315) {
         text_area.textContent = "パラダイス";
       }
+
+// in between hawaii and continental us (california?)
+      if (compassHeading > 315 || compassHeading < 45) {
+        text_area.textContent = "寒い";
+      }
+// in between california and minnesota
+      if (compassHeading >= 45 && compassHeading < 135) {
+        text_area.textContent = "帰場所";
+      }
+// in between minnesota and chicago
+      if (compassHeading >= 135 && compassHeading < 225) {
+        text_area.textContent = "熱い";
+      }
+// in between mn/chi and japan
+      if (compassHeading >= 225 && compassHeading <= 315) {
+        text_area.textContent = "パラダイス";
+    }
+
        //calls function to alter content based on heading
        // myOrientation();
-  };
+     };
 
 //device orientation that can track orientation on space
 	window.addEventListener('deviceorientation', oriented, false);
